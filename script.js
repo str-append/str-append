f6f3b7fea844d8abf199493c443e6fdb3ebe85d8
Yash Sharma <yashsharma@Yashs-MacBook-Air.local>
2022-01-10 11:00:26 +0530

    script for scheduler

---
+++ script.js
@@ -0,0 +1,31 @@
+const connectDB = require("./config/db");
+const File = require("./models/files");
+const fs = require("fs");
+
+connectDB();
+
+async function deleteData() {
+  const pastdate = new Date(Date.now() - 24 * 60 * 60 * 1000);
+  console.log(`date : ${Date.now()}`);
+  console.log(`past Date : ${pastdate}`);
+  const files = await File.find({ createdAt: { $lt: pastdate } });
+  //   if (files.length) {
+  //     for (const file of files) {
+  //       console.log(file);
+  //     }
+  //   }
+
+  if (files.length) {
+    for (const file of files) {
+      try {
+        fs.unlinkSync('https://flsharefilesharingapp.herokuapp.com/file.path');
+        await file.remove();
+        console.log(`File removed succesfully ${file.filename}`);
+      } catch (err) {
+        console.log(`error in deleting ${err}`);
+      }
+    }
+  }
+}
+
+deleteData().then(process.exit);
